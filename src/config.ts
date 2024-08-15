ebf38dd99838b646cdf6702bb0d5e34e09efa012
0xkakashi <locknauthkavish@gmail.com>
2024-08-15 23:54:26 +0400

    looks good

--- src/config.ts
+++ src/config.ts
@@ -7,16 +7,16 @@ import type {
 import { LinkPreset } from './types/config'
 
 export const siteConfig: SiteConfig = {
-  title: 'Fuwari',
-  subtitle: 'Demo Site',
+  title: '0xkakashi',
+  subtitle: 'Home',
   lang: 'en',         // 'en', 'zh_CN', 'zh_TW', 'ja', 'ko'
   themeColor: {
-    hue: 250,         // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
-    fixed: false,     // Hide the theme color picker for visitors
+    hue: 220,         // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
+    fixed: true,     // Hide the theme color picker for visitors
   },
   banner: {
-    enable: false,
-    src: 'assets/images/demo-banner.png',   // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+    enable: true,
+    src: 'assets/images/banner.jpeg',   // Relative to the /src directory. Relative to the /public directory if it starts with '/'
     position: 'center', // Equivalent to object-position, defaults center
     credit: {
       enable: false,         // Display the credit text of the banner image
@@ -25,11 +25,10 @@ export const siteConfig: SiteConfig = {
     }
   },
   favicon: [    // Leave this array empty to use the default favicon
-    // {
-    //   src: '/favicon/icon.png',    // Path of the favicon, relative to the /public directory
-    //   theme: 'light',              // (Optional) Either 'light' or 'dark', set only if you have different favicons for light and dark mode
-    //   sizes: '32x32',              // (Optional) Size of the favicon, set only if you have favicons of different sizes
-    // }
+    {
+      src: '/favicon/kakashi-favicon.png',    // Path of the favicon, relative to the /public directory              // (Optional) Either 'light' or 'dark', set only if you have different favicons for light and dark mode
+      sizes: '32x32',              // (Optional) Size of the favicon, set only if you have favicons of different sizes
+    }
   ]
 }
 
@@ -38,41 +37,41 @@ export const navBarConfig: NavBarConfig = {
     LinkPreset.Home,
     LinkPreset.Archive,
     LinkPreset.About,
-    {
-      name: 'GitHub',
-      url: 'https://github.com/saicaca/fuwari',     // Internal links should not include the base path, as it is automatically added
-      external: true,                               // Show an external link icon and will open in a new tab
-    },
+    // {
+    //   name: 'GitHub',
+    //   url: '',     // Internal links should not include the base path, as it is automatically added
+    //   external: true,                               // Show an external link icon and will open in a new tab
+    // },
   ],
 }
 
 export const profileConfig: ProfileConfig = {
-  avatar: 'assets/images/demo-avatar.png',  // Relative to the /src directory. Relative to the /public directory if it starts with '/'
-  name: 'Lorem Ipsum',
-  bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+  avatar: 'assets/images/kakashi-avatar.png',  // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+  name: '0xkakashi',
+  bio: 'CyberSOC Analyst | CTF Player',
   links: [
     {
-      name: 'Twitter',
-      icon: 'fa6-brands:twitter',       // Visit https://icones.js.org/ for icon codes
+      name: 'Gmail',
+      icon: 'fa6-solid:envelope',       // Visit https://icones.js.org/ for icon codes
                                         // You will need to install the corresponding icon set if it's not already included
                                         // `pnpm add @iconify-json/<icon-set-name>`
-      url: 'https://twitter.com',
+      url: 'mailto:keshav.locknauth@hotmail.com',
     },
     {
-      name: 'Steam',
-      icon: 'fa6-brands:steam',
-      url: 'https://store.steampowered.com',
+      name: 'LinkedIn',
+      icon: 'fa6-brands:linkedin',
+      url: 'https://www.linkedin.com/in/keshav-locknauth/',
     },
     {
       name: 'GitHub',
       icon: 'fa6-brands:github',
-      url: 'https://github.com/saicaca/fuwari',
+      url: 'https://github.com/kes-hav',
     },
   ],
 }
 
 export const licenseConfig: LicenseConfig = {
-  enable: true,
+  enable: false,
   name: 'CC BY-NC-SA 4.0',
   url: 'https://creativecommons.org/licenses/by-nc-sa/4.0/',
 }
--- src/content/spec/about.md
+++ src/content/spec/about.md
@@ -1,9 +1,14 @@
-# About
-This is the demo site for [Fuwari](https://github.com/saicaca/fuwari).
+# About Me
+👋 Hi, I’m Keshav Locknauth aka **0xkakashi**, a passionate cybersecurity professional.
 
-::github{repo="saicaca/fuwari"}
+🌱 My interests span across the vast realm of IT, with a keen focus on Cyber Security. I love learning and exploring new technologies. I actively compete in various Capture The Flag (CTF) competitions with my team, **konohagakure**. You can find out more about us on our [CTFtime profile](https://ctftime.org/team/309244)
 
-> ### Sources of images used in this site
-> - [Unsplash](https://unsplash.com/)
-> - [星と少女](https://www.pixiv.net/artworks/108916539) by [Stella](https://www.pixiv.net/users/93273965)
-> - [Rabbit - v1.4 Showcase](https://civitai.com/posts/586908) by [Rabbit_YourMajesty](https://civitai.com/user/Rabbit_YourMajesty)
+💻 My tech journey has equipped me with a robust skill set in web development and programming. I’ve worked with technologies including HTML, CSS, JavaScript, TypeScript, Tailwind CSS, React, PHP, Firebase, SQL, NoSQL, MongoDB, Python, C, Java, C++, Flask, and Django. This diverse knowledge allows me to integrate security with development seamlessly.
+
+🌟 Beyond cybersecurity and coding, I’m constantly exploring new technologies and staying updated with the latest industry trends.
+
+🌀 I am a big fan of Naruto, especially **Kakashi Hatake**. Kakashi's dedication, resilience, and strategic thinking are a huge source of inspiration for me. His ability to overcome challenges with determination and skill motivates me to approach my own endeavors with the same level of commitment and creativity. Kakashi’s journey reminds me to stay focused, adapt, and never stop learning, qualities that I strive to embody in both my personal and professional life.🥷
+
+📫 Feel free to connect with me on [LinkedIn](https://www.linkedin.com/in/keshav-locknauth/) or reach out via [email](mailto:keshav.locknauth@hotmail.com).
+
+![Kakashi Image](../../../public/favicon/susanoo.png)
